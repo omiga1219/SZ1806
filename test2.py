96a7f2fef24c5e2165cce9d659f3e256428c11a7
omiga1219 <441756791@qq.com>
2018-07-27 10:02:32 +0800

    [新增]修改了test2,omiga

--- test2.py
+++ test2.py
@@ -4,7 +4,7 @@ print("hello world!")
 def max(num1,num2):
     return num1+num2
 
-num1 = input('输入数字1：')
-num2 = input('输入数字2：')
+num1 = int(input('输入数字1：'))
+num2 = int(input('输入数字2：'))
 res=max(num1,num2)
 print(res)
